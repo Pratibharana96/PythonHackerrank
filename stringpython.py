96101171e587c82f23e8870aa95f3b8e5cd2a911
Pratibha Rana <54569773+Pratibharana96@users.noreply.github.com>
2020-01-21 15:06:31 +0530

    Create stringpython.py

---
+++ stringpython.py
@@ -0,0 +1,86 @@
+We check in a student name Ruchi . she is 15 years old , and is a new student .
+
+var name= 'ruchi'
+var age = 15
+is_new  = True
+
+//input take
+name=input('what is your name?')
+fav_col =input('What is your fav color')
+print('hi '  +name + ' your favorite color is' + fav_col )
+.......................................................................................................................................
+
+                                           Important Questions in PYTHON
+                              Basics
+                              1.What is an expression?
+                              2.What is a syntax error?
+                              3.What is PEP8?
+                              4.What does a linter do?
+                              5.What is the result of this expression: “*” * 10
+                              6.What is CPython?
+                              7.How is CPython different from Jython?
+                              8.How is CPython different from IronPython?
+                              9.Primitive Types
+                              10.What is a variable?
+                              11.What are the primitive built-in types in Python?
+                              12.When should we use “”” (tripe quotes) to define strings?
+                              13.Assuming (name = “John Smith”), what does name[1] return?
+                              14.What about name[-2]?
+                              15.What about name[1:-1]?
+                              16.How to get the length of name?
+                              17.What are the escape sequences in Python?
+                              19.What is the result of f“{2+2}+{10%3}”?
+                              20.Given (name = “john smith”), what will name.title() return?
+                              21.What does name.strip() do?
+                              22.What will name.find(“Smith”) return?
+                              23.What will be the value of name after we call name.replace(“j”, “k”)?
+                              24.How can we check to see if name contains “John”?
+                              25.What are the 3 types of numbers in Python?
+                              Control Flow
+                              26.What is the difference between 10 / 3 and 10 // 3?
+                              27.What is the result of 10 ** 3?
+                              28.Given (x = 1), what will be the value of after we run (x += 2)?
+                              29.How can we round a number?
+                              What is the result of float(1)?
+                              What is the result of bool(“False”)?
+                              What are the falsy values in Python?
+                              What is the result of 10 == “10”?
+                              What is the result of “bag” > “apple”?
+                              What is the result of not(True or False)?
+                              Under what circumstances does the expression 18 <= age < 65 evaluate to True?
+                              What does range(1, 10, 2) return?
+                              Name 3 iterable objects in Python.
+                              Functions
+                              What is the difference between a parameter and an argument?
+                              All functions in Python by default return …?
+                              What are keyword arguments and when should we use them?
+                              How can we make a parameter of a function optional?
+                              What happens when we prefix a parameter with an asterisk (*)?
+                              What about two asterisks (**)?
+                              What is scope?
+                              What is the difference between local and global variables?
+                              Why is using the global statement a bad practice?
+                              Coding Exercises
+                              Write a function that returns the maximum of two numbers.
+                              Write a function called fizz_buzz that takes a number.
+                              If the number is divisible by 3, it should return “Fizz”.
+                              If it is divisible by 5, it should return “Buzz”.
+                              If it is divisible by both 3 and 5, it should return “FizzBuzz”.
+                              Otherwise, it should return the same number.
+                              Write a function for checking the speed of drivers. This function should have one parameter: speed.
+                              If speed is less than 70, it should print “Ok”.
+                              Otherwise, for every 5km above the speed limit (70), it should give the driver one demerit point and print the total number of demerit points. For example, if the speed is 80, it should print: “Points: 2”.
+                              If the driver gets more than 12 points, the function should print: “License suspended”
+                              Write a function called showNumbers that takes a parameter called limit. It should print all the numbers between 0 and limit with a label to identify the even and odd numbers. For example, if the limit is 3, it should print:
+                              0 EVEN
+                              1 ODD
+                              2 EVEN
+                              3 ODD
+                              Write a function that returns the sum of multiples of 3 and 5 between 0 and limit (parameter). For example, if limit is 20, it should return the sum of 3, 5, 6, 9, 10, 12, 15, 18, 20.
+                              Write a function called show_stars(rows). If rows is 5, it should print the following:
+                              *
+                              **
+                              ***
+                              ****
+                              *****
+                              Write a function that prints all the prime numbers between 0 and limit where limit is a parameter.
